da9869d17dfacd77577ebf205957776f0368b809
Bolsyn <Serikagis@yandex.ru>
2021-02-10 09:14:19 +0600

    Add project files.

---
+++ DelegateHomeWork/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DelegateHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var button = new Button
+            {
+                Length = 15,
+                Height = 6,
+                Text = "Welcome",
+                Exec = new RunAnyPara(Console.WriteLine)
+            };
+
+            button.PrintButton();
+
+            button.Exec(2);
+            button.Exec("Name");
+        }
+    }
+}
